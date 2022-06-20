810df5230430c14ec90d0ebd4c04a1886ad0958e
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-06-20 16:04:23 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Costruisco il mio array di object relativi ai singoli post
+const postArray = [
+  {
+    id: 1,
+    name: "Gianni",
+    lastName: "Morandi",
+    date: "06-20-2022",
+    text: "Fatti mandare dalla mamma a prendere il latte",
+    image: 
+  },
+];
